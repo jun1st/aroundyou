cd04d1b5803d6dc821ddb247004e37c5a1ec10da
jun1st-office <jun1st.feng@gmail.com>
2011-12-19 16:29:34 +0800

    move message get function from controller to model

--- application/controllers/message.php
+++ application/controllers/message.php
@@ -1,123 +1,119 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-	include_once $_SERVER['DOCUMENT_ROOT'] . '/application/models/message_model.php';
-	include_once $_SERVER['DOCUMENT_ROOT'] . '/application/models/comment_model.php';
-	include_once $_SERVER['DOCUMENT_ROOT'] . '/application/models/region_model.php';
-	//include_once $_SERVER['DOCUMENT_ROOT'] . '/application/helpers/relatime_time.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/application/models/message_model.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/application/models/comment_model.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/application/models/region_model.php';
+//include_once $_SERVER['DOCUMENT_ROOT'] . '/application/helpers/relatime_time.php';
 
-	class Message extends My_Controller
-	{
-		function __contruct()
-		{
-			parent::__construct();
-		}
-		public function index()
-		{
-			$this->load->helper('date');
-			$this->load->Model('Message_model');
-			$this->load->Model('Region_model');
+class Message extends My_Controller
+{
+  function __contruct()
+  {
+    parent::__construct();
+  }
+  public function index()
+  {
+    $this->load->helper('date');
+    $this->load->Model('Message_model');
+    $this->load->Model('Region_model');
 
-			$data['messages'] = $this->Message_model->get_messages();
-			$data['regions'] = $this->Region_model->get_regions();
+    $data['messages'] = $this->Message_model->get_messages();
+    $data['regions'] = $this->Region_model->get_regions();
 			
-			$this->load->view('Message/index', $data);
-		}
+    $this->load->view('Message/index', $data);
+  }
 		
-		public function get_by_region($region_name)
-		{
-			$this->load->helper('date');
-			$this->load->Model('Message_model');
-			$this->load->Model('Region_model');
+  public function get_by_region($region_name)
+  {
+    $this->load->helper('date');
+    $this->load->Model('Message_model');
+    $this->load->Model('Region_model');
 			
-			$region = $this->Region_model->get_region_by_name($region_name);
+    $region = $this->Region_model->get_region_by_name($region_name);
 			
-			$data['messages'] = $this->Message_model->get_messages_by_region($region->id);
-			$data['regions'] = $this->Region_model->get_regions();
+    $data['messages'] = $this->Message_model->get_messages_by_region($region->id);
+    $data['regions'] = $this->Region_model->get_regions();
 			
-			$this->load->view('Message/index', $data);
-		}
+    $this->load->view('Message/index', $data);
+  }
 		
-		public function view($id)
-		{	
-			$this->load->helper('date');
-			$this->db->select("messages.id as message_id, topic, content, posted_time, users.id as user_id, users.name as user_name,users.description as user_description, users.profile_tiny_image_path as user_profile_image, regions.name as region_name");
-			$this->db->from("messages");
-			$this->db->join("users", 'messages.user_id = users.id');
-			$this->db->join('message_region', 'messages.id = message_region.message_id', 'left');
-			$this->db->join('regions', 'message_region.region_id = regions.id', 'left');
-			$this->db->where('messages.id', $id);
-			$data['message'] = $this->db->get()->row();
+  public function view($id)
+  {	
+    $this->load->helper('date');
+    
+    $this->load->Model('Message_model');
+    
+    $data['message'] = $this->Message_model->get_message_detail($id);
 			
-			
-			if ($data['message'] == null) {
-				echo 'not found';
-			}
-			else
-			{
-				$this->db->select("content, posted_time, users.name as user_name,users.description as user_description, users.profile_tiny_image_path as profile_image");
-				$this->db->from('comments');
-				$this->db->join('users', 'user_id = users.id');
-				$this->db->where('message_id', $id);
-				$data['comments'] = $this->db->get()->result();
+    if ($data['message'] == null) {
+      echo 'not found';
+    }
+    else
+    {
+      $this->db->select("content, posted_time, users.name as user_name,users.description as user_description, users.profile_tiny_image_path as profile_image");
+      $this->db->from('comments');
+      $this->db->join('users', 'user_id = users.id');
+      $this->db->where('message_id', $id);
+      $data['comments'] = $this->db->get()->result();
 				
-				$this->load->view('Message/view', $data);
-			}
-		}
+      $this->load->view('Message/view', $data);
+    }
+  }
 		
-		public function add()
-		{
+  public function add()
+  {
 
-			if($this->session->userdata('is_login') != 'true')
-				redirect('login/');
+    if($this->session->userdata('is_login') != 'true')
+    redirect('login/');
 			
-			if(isset($_POST['submit']))
-			{
-				$this->load->library('form_validation');
+    if(isset($_POST['submit']))
+    {
+      $this->load->library('form_validation');
 				
-				$this->form_validation->set_rules('content', '内容', 'required|max_length[140]');
-				$this->form_validation->set_rules('regions', '地标', 'required');
-				if ($this->form_validation->run() == TRUE) 
-				{
+      $this->form_validation->set_rules('content', '内容', 'required|max_length[140]');
+      $this->form_validation->set_rules('regions', '地标', 'required');
+      if ($this->form_validation->run() == TRUE) 
+      {
 				
-					$region_name = $this->input->post('regions');
-					$this->load->Model('Region_model');
-					$region = $this->Region_model->get_region_by_name($region_name);
-					if ($region == null) {
-						$region_id = $this->Region_model->add_region($region_name);
-					}else {
-						$region_id = $region->id;
-					}
+        $region_name = $this->input->post('regions');
+        $this->load->Model('Region_model');
+        $region = $this->Region_model->get_region_by_name($region_name);
+        if ($region == null) {
+          $region_id = $this->Region_model->add_region($region_name);
+        }else {
+          $region_id = $region->id;
+        }
 					
-					$topic = $this->input->post('topic');
-					$content = $this->input->post('content');
-					$user_id = $this->session->userdata('user')->id;
+        $topic = $this->input->post('topic');
+        $content = $this->input->post('content');
+        $user_id = $this->session->userdata('user')->id;
 				
-					$this->load->Model('Message_model');
-					$message_id = $this->Message_model->add_message($topic, $content, $user_id, $region_id);
+        $this->load->Model('Message_model');
+        $message_id = $this->Message_model->add_message($topic, $content, $user_id, $region_id);
 					
-					redirect('/message');
-				}
-			}
+        redirect('/message');
+      }
+    }
 			
-			$data['user'] = $this->session->userdata('user');
+    $data['user'] = $this->session->userdata('user');
 			
-			$this->load->view('Message/add', $data);
-		}
+    $this->load->view('Message/add', $data);
+  }
 		
-		public function comment()
-		{
-			if (isset($_POST['submit'])) {
+  public function comment()
+  {
+    if (isset($_POST['submit'])) {
 				
-				$comment = new Comment_model;
-				$comment->message_id = $this->input->post('message_id');
-				$comment->user_id = $this->session->userdata('user')->id;
-				$comment->content = $this->input->post('comment_content');
-				$comment->posted_time = date('Y-m-d H:i:s');
+      $comment = new Comment_model;
+      $comment->message_id = $this->input->post('message_id');
+      $comment->user_id = $this->session->userdata('user')->id;
+      $comment->content = $this->input->post('comment_content');
+      $comment->posted_time = date('Y-m-d H:i:s');
 				
-				$this->db->insert('comments', $comment);
+      $this->db->insert('comments', $comment);
 				
-				redirect("message/view/$comment->message_id");
-			}
-		}
-	}
+      redirect("message/view/$comment->message_id");
+    }
+  }
+}
 
 ?>
