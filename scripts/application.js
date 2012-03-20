54d50e4fb0657936895a5e94ab41c361481a7297
jun1st-office <jun1st.feng@gmail.com>
2012-03-20 17:47:48 +0800

    add font-awesome css file

--- scripts/application.js
+++ scripts/application.js
@@ -1,6 +1,6 @@
 
-$(function(){
-	var url = window.location.toString();
-	var segment = url.substring(url.lastIndexOf("#"));
-	$("#user-setting-navs a[href=" + segment + "]").tab('show');
-});
+// $(function(){
+//     var url = window.location.toString();
+//     var segment = url.substring(url.lastIndexOf("#"));
+//     $("#user-setting-navs a[href=" + segment + "]").tab('show');
+// });
