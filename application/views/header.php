d2f71857c03362c6a31233646a248c04187d90df
jun1st-office <jun1st.feng@gmail.com>
2012-06-07 10:37:13 +0800

    add "viewport" metadata

--- application/views/header.php
+++ application/views/header.php
@@ -1,4 +1,4 @@
-
+<meta name="viewport" content="width=device-width" />
 <link rel="stylesheet" href="/css/bootstrap.css" type="text/css" media="screen" title="no title" charset="utf-8">
 <link rel="stylesheet" href="/css/site.css" type="text/css" media="screen" title="no title" charset="utf-8">
 
